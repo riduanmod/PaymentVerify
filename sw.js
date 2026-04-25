911dc34fa40d8c1023d99a79bf59d4eebd5de984
riduanmod <riduanmod@gmail.com>
2026-04-25 11:25:16 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'payhub-v2';
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
